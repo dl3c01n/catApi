fe3ac13a5bf9f980131494595094b657167f34ae
dl3c01n <david.lecoin@yahoo.com>
2020-10-28 13:38:46 +0100

    display name in modal

--- assets/js/catService.js
+++ assets/js/catService.js
@@ -1,9 +1,23 @@
-let catUrl;
-
 const getNewCat = () => {
     fetch("https://api.thecatapi.com/v1/images/search")
     .then(resp=>resp.json())
     .then(json=>json.forEach(cat => {
         setImageUrl(json[0].url);
+    }))
+}
+
+const displayInfo = () => {
+    fetch("https://api.thecatapi.com/v1/images/search")
+    .then(resp=>resp.json())
+    .then(json=>json.forEach(cat => {
+        console.log(cat);
+        if(cat.breeds.length>0){
+            if(cat.breeds[0].hasOwnProperty('name')){
+                setInfos(cat.breeds[0].name);
+            }
+        }else{
+            setInfos("Il n'y pas de détails disponibles");
+        }
+        
     }))
 }
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, {});
+});
+
 const setImageUrl = (url) => {
     document.querySelector('#catImg').src = url;
 }
 
+const setInfos = (infos) => {
+    document.querySelector('#catDetails').textContent = infos.toString();
+}
+
 window.addEventListener('load', getNewCat);
+window.addEventListener('load', displayInfo);
 document.querySelector('#buttonNext').addEventListener('click', getNewCat)
+document.querySelector('#buttonNext').addEventListener('click', displayInfo)
